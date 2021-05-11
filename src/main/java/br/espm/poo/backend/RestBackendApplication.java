f527e9badafb282ce6a1a983b703f0867fa79ac1
Raphael Menezes de Jesus <61888147+RaphaelJesus1@users.noreply.github.com>
2021-05-10 21:07:10 -0300

    Environment configuration

--- src/main/java/br/espm/poo/backend/RestBackendApplication.java
+++ src/main/java/br/espm/poo/backend/RestBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExemploDeBackendApplication {
+public class RestBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExemploDeBackendApplication.class, args);
+		SpringApplication.run(RestBackendApplication.class, args);
 	}
 
 }
